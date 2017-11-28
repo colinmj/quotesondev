29235975a50a2ac41a3af3299baea2f057b20640
Colin MJ <colinmj@telus.net>
2017-11-28 15:03:51 -0800

    added Read me

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
-# Quotes on Dev Starter
+## Quotes On Dev 
 
-A WordPress starter theme for the Quotes on Dev project, forked from Underscores.
+I've replicated the designers vision for 'Quotes on Dev'- a site inspired by Chris Coyier's "Quotes on Design". Quotes on Dev uses a custom Rest Api to randomly fetch a different post when the 'Show me another' button is clicked.
 
-## Installation
+## Getting Started
 
-### 1. Download me (don't clone me!)
+The home page displays a quote and a button that one can click to generate another random quote. Pressing this button initiates an ajax request to the rest api to display a new quote from the quotes stored in the blog posts section of the site. There is an About page, and Archives page that has direct links to the different authors, categories and tags, and there is a submit page. If the user is logged in they can submit a new quote via an ajax post request.
 
-Then add me to your `wp-content/themes` directory.
+## Built With
 
-### 2. Rename the `quotesondev-starter-master` directory
+* Html 5
+* PHP
+* Wordpress
+* CSS
+* SCSS
+* JavaScript
+* jQuery
+* Gulp
+* Eslint
+* Pretty Error
 
-Make sure that the theme directory name is project appropriate! Do you need `starter` or `master` in the directory name?
+## Learning 
 
-### 3. Install the dev dependencies
+This was my first experience working with a custom api, and using ajax with wordpress. As it's only my second wordpress site, I'm still learning about the template hierarchy and all the built in wordpress methods. This was also my first ajax post request.
 
-Next you'll need to run `npm install` **inside your theme directory** to install the npm packages you'll need for Gulp, etc.
+## Author 
 
-### 4. Update the proxy in `gulpfile.js`
-
-Lastly, be sure to update your `gulpfile.js` with the appropriate URL for the Browsersync proxy (so change `localhost[:port-here]/[your-dir-name-here]` to the appropriate localhost URL).
-
-And now would be a good time to `git init` :)
-# quotesondev
+Colin Matson-Jones
--- page-submit.php
+++ page-submit.php
@@ -1,66 +1,66 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * @package QOD_Starter_Theme
- */
+* The template for displaying all pages.
+*
+* @package QOD_Starter_Theme
+*/
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+<main id="main" class="site-main" role="main">
 
-    <div class="form-quote-wrapper"> 
+<div class="form-quote-wrapper"> 
 
-    <div class="left-quote">
-				<i class="fa fa-quote-left" aria-hidden="true"></i>
-      </div> <!-- closes left quote -->
+<div class="left-quote">
+<i class="fa fa-quote-left" aria-hidden="true"></i>
+</div> <!-- closes left quote -->
 
-      <div class="form-wrapper">
+<div class="form-wrapper">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-			<?php endwhile; // End of the loop. ?>
+<?php endwhile; // End of the loop. ?>
 
-      <?php  if( is_user_logged_in() && current_user_can( 'edit_posts' ) ): ?>
-
-     
-
-    
-   
-
-      <form name="submit-form" class="submit-form">
-      
-
-       <div class="form-author" >
-          <label> Author of Quote</label>
-          <input class="name" type="text">
-      </div>
-
-        <div class="form-quote">
-          <label> Quote </label>
-          <textarea rows="3" cols="20" class="content-form" type="text"></textarea>
-        </div>
-
-        <div class="form-where" >
-          <label>Where did you find thid quote </label>
-          <input class="source-form" type="text">
-        </div>
-
-        <div class="form-url">
-          <label> Provide a URL of the source of the quote</label>
-          <input class="source-url" type="url">
-        </div>
-
-        <button type="button" class="post-submit">Submit Quote</button>
-      </form>
-    </div> <!-- closes form wrapper -->
+<?php  if( is_user_logged_in() && current_user_can( 'edit_posts' ) ): ?>
 
 
-    <div class="right-quote">
-			  <i class="fa fa-quote-right" aria-hidden="true"></i>
-    </div><!-- closes right-->
+
+
+
+
+<form name="submit-form" class="submit-form">
+
+
+<div class="form-author" >
+<label> Author of Quote</label>
+<input class="name" type="text">
+</div>
+
+<div class="form-quote">
+<label> Quote </label>
+<textarea rows="3" cols="20" class="content-form" type="text"></textarea>
+</div>
+
+<div class="form-where" >
+<label>Where did you find thid quote </label>
+<input class="source-form" type="text">
+</div>
+
+<div class="form-url">
+<label> Provide a URL of the source of the quote</label>
+<input class="source-url" type="url">
+</div>
+
+<button type="button" class="post-submit">Submit Quote</button>
+</form>
+</div> <!-- closes form wrapper -->
+
+
+<div class="right-quote">
+<i class="fa fa-quote-right" aria-hidden="true"></i>
+</div><!-- closes right-->
 </div> <!-- closes form quote wrapper-->
 
 <?php else: ?>
@@ -72,14 +72,14 @@ get_header(); ?>
 </div> <!--closes form wrapper -->
 
 <div class="right-quote">
-			  <i class="fa fa-quote-right" aria-hidden="true"></i>
-    </div><!-- closes right-->
+<i class="fa fa-quote-right" aria-hidden="true"></i>
+</div><!-- closes right-->
 </div> <!-- closes for quote wrapper-->
 
 <?php endif; ?>
 
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
